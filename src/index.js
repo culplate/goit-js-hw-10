b87859382a1354b903f5aaf96477cf213f8f340b
culplate <culplate@gmail.com>
2023-12-13 20:12:17 +0100

    Added function to fetch chosen breed

--- src/index.js
+++ src/index.js
@@ -2,27 +2,44 @@ import axios from "axios";
 // auth api key
 axios.defaults.headers.common["x-api-key"] = "live_bzX9zMlIdP0XbVHw6qcJXC7BqgocB40pbl5Xvb0eXAEI2Aqcx0CzHBkdhfHhppgr";
 
+const loadingMes = document.querySelector('.loader');
+const errorMes = document.querySelector('.error');
 const dropdown = document.querySelector('.breed-select');
+const contentCont = document.querySelector('.cat-info');
+dropdown.addEventListener('change', handleSelect);
 
 fetchBreeds()
     .then(res => {
-        console.log(res); // remove later
-        dropdown.insertAdjacentHTML('beforeend', createOptions(res.data));
-        })
-    .catch(err => console.log(err.code))
+        dropdown.insertAdjacentHTML('beforeend', addOptions(res.data));
+    })
+    .catch(err => {
+        console.log(err.code) //handle error!!!!
+    })
 
 // markup for options
-function createOptions(arr) {
+function addOptions(arr) {
     return arr.map(({ id, name }) => {
         return `<option value="${id}">${name}</option>`
     }).join("");
 }
 
+function markupContent(arr) {
+
+}
+
+function handleSelect() {
+    console.log(dropdown.value);
+    fetchCatByBreed(dropdown.value)
+        .then(res => console.log(res.data[0]))
+        .catch(err => console.log(err.message)) // handle error!!!!
+}
+
+
 // move to and then import from cat-api.js
 function fetchBreeds() {
-    return axios.get("https://api.thecatapi.com/v1/breeds")
-        .then(resp => {
-            if (!resp.status === 200) throw new Error(error);
-            return resp;
-        })
+    return axios.get("https://api.thecatapi.com/v1/breeds");
+}
+
+function fetchCatByBreed(breedId) {
+    return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`);
 }
