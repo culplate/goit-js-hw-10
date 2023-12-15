a8c8dc9e3ebb39e550658449f6d7e54d1beef03e
culplate <culplate@gmail.com>
2023-12-15 17:59:02 +0100

    UI improvements

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import axios from "axios";
+import Notiflix from "notiflix";
 // auth api key
 axios.defaults.headers.common["x-api-key"] = "live_bzX9zMlIdP0XbVHw6qcJXC7BqgocB40pbl5Xvb0eXAEI2Aqcx0CzHBkdhfHhppgr";
+
 // TO DO:
-// 1. try putting loading and error messages handling in separate functions to clean the code
-// 2. add custom loaders
-// 3. Notiflix for error message
-// 4. add custom dropdown
+// 2. add custom loaders (not required)
+// 4. add custom dropdown (not required)
 
 const loadingMes = document.querySelector('.loader');
-const errorMes = document.querySelector('.error');
 const dropdown = document.querySelector('.breed-select');
 const contentCont = document.querySelector('.cat-info');
 
-// markup for options
 function addOptions(arr) {
     return arr.map(({ id, name }) => {
         return `<option value="${id}">${name}</option>`
@@ -23,21 +21,21 @@ function markupContent(obj) {
     const { url, breeds: { 0: { name, description, origin, life_span, temperament, wikipedia_url } } } = obj;
         return `
             <img class="image" src="${url}" alt="${name}">
-            <h1 class="main-title">${name}</h1>
-            <p class="description">${description}</p>
-            <h2 class="title">Info:</h2>
-            <ul class="list">
-                <li class"list-item">Origin: ${origin}</li>
-                <li class"list-item">Lifespan: ${life_span}</li>
-                <li class"list-item">Temperament: ${temperament}</li>
-            </ul>
-            <a class="link" href="${wikipedia_url}" target="_blank">Read more</a>
+            <div class="content-wrapper">
+                <h1 class="main-title">${name}</h1>
+                <p class="description">${description}</p>
+                <ul class="list">
+                    <li class="list-item"><strong>Origin:</strong> ${origin}</li>
+                    <li class="list-item"><strong>Lifespan:</strong> ${life_span}</li>
+                    <li class="list-item"><strong>Temperament:</strong> ${temperament}</li>
+                </ul>
+                <a class="link" href="${wikipedia_url}" target="_blank">Read more</a>
+            </div>
         `
 }
 
 function handleSelect() {
-    errorMes.hidden = true;
-    loadingMes.hidden = false;
+    showElement(loadingMes);
     contentCont.innerHTML = '';
     fetchCatByBreed(dropdown.value)
         .then(res => {
@@ -45,25 +43,31 @@ function handleSelect() {
         })
         .catch(err => {
             console.log(err.message)
-            errorMes.hidden = false; // change  those to css class "isHidden"
-        }) // handle error!!!!
-        .finally(() => loadingMes.hidden = true)
+            Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!')
+        })
+        .finally(() => hideElement(loadingMes))
+}
+
+function hideElement(elem) {
+    elem.classList.add('is-hidden');
+}
+
+function showElement(elem) {
+    elem.classList.remove('is-hidden');
 }
 
 dropdown.addEventListener('change', handleSelect);
-dropdown.hidden = true; // change those to css class "isHidden"
-errorMes.hidden = true; 
 
 fetchBreeds()
     .then(res => {
         dropdown.insertAdjacentHTML('beforeend', addOptions(res.data));
-        dropdown.hidden = false;
+        showElement(dropdown);
     })
     .catch(err => {
         console.log(err.message)
-        errorMes.hidden = false; // change those to css class "isHidden"
+        Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!')
     })
-    .finally(() => loadingMes.hidden = true)
+    .finally(() => hideElement(loadingMes))
 
 // move to and then import from cat-api.js
 function fetchBreeds() {
